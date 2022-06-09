89f35c8d427ecd4ab2493d48b6abcb0d0cba8398
fansurirazak <45352643+fansurirazak@users.noreply.github.com>
2022-06-09 14:31:43 +0800

    Password hashing attempt #1
    added password hashing functions

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,7 +23,8 @@ namespace ManagementSystem.Controllers
         {
             using(ManagementSystemEntities db = new ManagementSystemEntities())
             {
-                var userDetails = db.tb_user.Where(x => x.IC == userModel.IC && x.Password == userModel.Password).FirstOrDefault();
+                
+                var userDetails = db.tb_user.Where(x => x.IC == userModel.IC && VerifyHashedPassword(x.Password, userModel.Password)).FirstOrDefault();
                
                 if(userDetails ==null)
                 {
@@ -41,6 +43,46 @@ namespace ManagementSystem.Controllers
             }
            
         }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            return ByteArraysEqual(buffer3, buffer4);
+        }
+
+        public static bool ByteArraysEqual(byte[] b1, byte[] b2)
+        {
+            if (b1 == b2) return true;
+            if (b1 == null || b2 == null) return false;
+            if (b1.Length != b2.Length) return false;
+            for (int i = 0; i < b1.Length; i++)
+            {
+                if (b1[i] != b2[i]) return false;
+            }
+            return true;
+        }
+
 
         public ActionResult Logout()
         {
