30d216ad548e7f42e33d66395290f41991c984e5
nurhadirahmunawarah <nurhadirahmunawarah@graduate.utm.my>
2022-06-15 00:42:36 +0800

    Pie Chart

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using ManagementSystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,8 +22,8 @@ namespace ManagementSystem.Controllers
             var numStudent = db.tb_student.Count();
             var numTutor = db.tb_user.Count();
             decimal salaryRate = db.tb_salaryRate.OrderByDescending(x => x.DateCreated).FirstOrDefault().SalaryRate.Value;
-            var ClassNow = db.tb_class.Where(a => a.Date >= DateTime.Now && a.TutorID== test).Count();
-            var ClassEnded = db.tb_class.Where(a => a.Date < DateTime.Now && a.verifyStatus==1 && a.TutorID == test).Count();
+            var ClassNow = db.tb_class.Where(a => a.Date >= DateTime.Now && a.TutorID == test).Count();
+            var ClassEnded = db.tb_class.Where(a => a.Date < DateTime.Now && a.verifyStatus == 1 && a.TutorID == test).Count();
 
             ViewBag.numStudent = numStudent;
             ViewBag.numTutor = numTutor;
@@ -61,5 +63,33 @@ namespace ManagementSystem.Controllers
 
             return View();
         }
+
+        public ActionResult GetData()
+        {
+
+
+            int adminCount = db.tb_user.Where(x => x.Role == 1).Count();
+            int tutorCount = db.tb_user.Where(x => x.Role == 2).Count();
+            int stuCount = db.tb_student.Count();
+
+            Ratio obj = new Ratio();
+            obj.Admin = adminCount;
+            obj.Tutor = tutorCount;
+            obj.Student = stuCount;
+
+
+            return Json(obj, JsonRequestBehavior.AllowGet);
+
+        }
+        public class Ratio
+        {
+            public int Admin
+            { get; set; }
+            public int Tutor
+            { get; set; }
+            public int Student
+            { get; set; }
+        }
+
     }
 }
---
+++ Scripts/Dashboard.js
@@ -0,0 +1,60 @@
+﻿// Build the chart
+
+$(document).ready(function () {
+        $.getJSON("/Home/GetData", function (data) {
+
+
+            Highcharts.chart('container', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Bilangan Pengguna Sistem ini'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y}</b>'
+                },
+                accessibility: {
+                    point: {
+                        valueSuffix: '%'
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    name: 'Pengguna',
+                    colorByPoint: true,
+                    data: [{
+                        name: 'Admin',
+                        y: data.Admin,
+                       
+                    }, {
+                        name: 'Tutor',
+                        y: data.Tutor
+                    }, {
+                        name: 'Student',
+                        y: data.Student
+                    }]
+                }]
+            });
+
+
+        });
+  
+});
+
+
+
+
+
