07b1e7fb24ca832e74a0a321b867e097b514b3e2
nurhadirahmunawarah <nurhadirahmunawarah@graduate.utm.my>
2022-06-14 15:40:21 +0800

    Latest Changes
    Edit on performance controller

--- Controllers/PerformanceController.cs
+++ Controllers/PerformanceController.cs
@@ -48,7 +48,7 @@ namespace ManagementSystem.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Remark,StudentID,DateCreated")] tb_performance tb_performance)
+        public ActionResult Create([Bind(Include = "ID,Remark,StudentID,DateCreated,ratingStudent")] tb_performance tb_performance)
         {
             if (ModelState.IsValid)
             {
@@ -82,7 +82,7 @@ namespace ManagementSystem.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Remark,StudentID,ratingStudent")] tb_performance tb_performance)
+        public ActionResult Edit([Bind(Include = "ID,Remark,StudentID,ratingStudent,ratingStudent")] tb_performance tb_performance)
         {
             if (ModelState.IsValid)
             {
